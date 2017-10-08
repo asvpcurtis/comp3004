141806024d3391efea4c4d2627417f933ef1e5bd
plzb0ss <curtisw0234@gmail.com>
2017-10-08 13:31:33 -0400

    Update connection string for aws rds instance

--- backend/TournamentMasterAPI/TournamentMasterDataAccess/TournamentMasterDataModel.Designer.cs
+++ backend/TournamentMasterAPI/TournamentMasterDataAccess/TournamentMasterDataModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Curtis Barlow-Wilkes\documents\visual studio 2015\Projects\TournamentMasterAPI\TournamentMasterDataAccess\TournamentMasterDataModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Curtis Barlow-Wilkes\Documents\comp3004\backend\TournamentMasterAPI\TournamentMasterDataAccess\TournamentMasterDataModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
