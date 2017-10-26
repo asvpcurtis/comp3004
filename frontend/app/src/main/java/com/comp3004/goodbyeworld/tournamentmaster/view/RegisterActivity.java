8a69b00a712ac757bde3174f92a391d6be58fa56
RINCEWIND\Michael <michaeljsouter@gmail.com>
2017-10-25 20:33:57 -0400

    Registration Complete

--- frontend/app/src/main/java/com/comp3004/goodbyeworld/tournamentmaster/view/RegisterActivity.java
+++ frontend/app/src/main/java/com/comp3004/goodbyeworld/tournamentmaster/view/RegisterActivity.java
@@ -1,6 +1,7 @@
 package com.comp3004.goodbyeworld.tournamentmaster.view;
 
 import android.content.DialogInterface;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,6 +12,7 @@ import android.widget.TextView;
 import com.amazonaws.mobileconnectors.cognitoidentityprovider.CognitoUser;
 import com.amazonaws.mobileconnectors.cognitoidentityprovider.CognitoUserAttributes;
 import com.amazonaws.mobileconnectors.cognitoidentityprovider.CognitoUserCodeDeliveryDetails;
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.handlers.GenericHandler;
 import com.amazonaws.mobileconnectors.cognitoidentityprovider.handlers.SignUpHandler;
 import com.comp3004.goodbyeworld.tournamentmaster.R;
 import com.comp3004.goodbyeworld.tournamentmaster.data.AppHelper;
@@ -28,6 +30,10 @@ public class RegisterActivity extends AppCompatActivity {
 
     public void registerClick(View view) {
         registerNewUser();
+    }
+
+    public void confirmClick(View view) {
+        confirmNewUser();
     }
 
     private void registerNewUser() {
@@ -49,12 +55,31 @@ public class RegisterActivity extends AppCompatActivity {
         cognitoUserAttributes.addAttribute("email", input.getText().toString());
 
         // add phone number
-        input = findViewById(R.id.editTextPhoneNumber);
+        //input = findViewById(R.id.editTextPhoneNumber);
         //cognitoUserAttributes.addAttribute("phone_number", input.getText().toString());
 
         AppHelper.getPool().signUpInBackground(username, password, cognitoUserAttributes, null, signUpHandler);
 
     }
+
+    private void confirmNewUser() {
+        username = ((TextView)findViewById(R.id.editTextUsername)).getText().toString();
+        String confirmCode = ((TextView)findViewById(R.id.editTextConfCode)).getText().toString();
+        AppHelper.getPool().getUser(username).confirmSignUpInBackground(confirmCode, true, confHandler);
+    }
+
+    GenericHandler confHandler = new GenericHandler() {
+        @Override
+        public void onSuccess() {
+            showDialogMessage("Success!",username+" has been confirmed!", true);
+            exit();
+        }
+
+        @Override
+        public void onFailure(Exception exception) {
+            showDialogMessage("Confirmation failed", AppHelper.formatException(exception), false);
+        }
+    };
 
     SignUpHandler signUpHandler = new SignUpHandler() {
         @Override
@@ -64,9 +89,11 @@ public class RegisterActivity extends AppCompatActivity {
             Boolean regState = signUpConfirmationState;
             if (signUpConfirmationState) {
                 // User is already confirmed
+                // Return to login screen
             }
             else {
                 // User is not confirmed
+                // Proceed to confirmation
             }
         }
 
@@ -96,5 +123,9 @@ public class RegisterActivity extends AppCompatActivity {
         });
         userDialog = builder.create();
         userDialog.show();
+    }
+
+    private void exit() {
+        NavUtils.navigateUpFromSameTask(this);
     }
 }
